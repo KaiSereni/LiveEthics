c74194bbb71f75c8a76b2081141e68643ee50744
KaiSereni <kaiserenior@gmail.com>
2025-03-05 10:42:08 -0800

    Update analyze.py

--- openboycottdata/analyze.py
+++ openboycottdata/analyze.py
@@ -313,9 +313,10 @@ def data_google(company_name: str, google_key: str, gemini_client: genai.Client,
             result = r.json()
             if "error" in result:
                 print(f"Result: {result}")
-            if not len(result['items']) > 0:
+            try:
+                assert len(result['items']) > 0
+            except:
                 print(f"No result items for {company_name} {description}")
-                break
             result_items = result["items"]
             print(f"Found {result_items.__len__()} Google sources for {company_name}")
             for item in result_items:
